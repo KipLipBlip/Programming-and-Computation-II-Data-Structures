c89711c22b72835e14c5d2ef9c41fb42d4d65090
dominic <dominicassia@gmail.com>
2020-09-19 15:14:39 -0400

    Fixed CGC sumbission

--- Code Golf/CGC1.py
+++ Code Golf/CGC1.py
@@ -1,3 +1,4 @@
+# 243 Characters
 def addQuotes(s):
     '''
         *Write a function that adds double quotes around lowercase words
@@ -21,17 +22,14 @@ def addQuotes(s):
             'NO CHANGE'
             >>> addQuotes('')
             ''
+            >>> addQuotes('a1')
+            '"a"1'
     '''
     x,y,i='',len(s),0
     while i!=y:
-        if s[i].isupper() or not s[i].isalnum(): x,i=x+s[i],i+1
+        if s[i].isupper() or not s[i].isalpha(): x,i=x+s[i],i+1
         else: 
             x+='"'
             while i!=y and s[i].islower(): x,i=x+s[i],i+1
             x+='"'
     return x
-
-if __name__ == "__main__":
-
-    import doctest
-    doctest.testmod()
