4da85cc48c310159bf0ff68868798b443ca2dbbd
dominic <dominicassia@gmail.com>
2020-09-30 10:24:33 -0400

    Working on missedChar()

--- Lab-3/LAB3.py
+++ Lab-3/LAB3.py
@@ -23,68 +23,44 @@ def skipping(n):
     else:        
         return skipping(n-2) + n
 
+
 def removing(aList):
     """
-    >>> removing([7, 4, 0])
-    [7, 4, 0]
-    >>> myList=[7, 4, -2, 1, 9]
-    >>> removing(myList)   # Found(-2) Delete -2 and 1
-    [7, 4, 9]
-    >>> myList
-    [7, 4, -2, 1, 9]
-    >>> removing([-4, -7, -2, 1, 9]) # Found(-4) Delete -4, -7, -2 and 1
-    [9]
-    >>> removing([-3, -4, 5, -4, 1])  # Found(-3) Delete -2, -4 and 5. Found(-4) Delete -4 and 1
-    []
+        >>> removing([7, 4, 0])
+        [7, 4, 0]
+        >>> myList=[7, 4, -2, 1, 9]
+        >>> removing(myList)   # Found(-2) Delete -2 and 1
+        [7, 4, 9]
+        >>> myList
+        [7, 4, -2, 1, 9]
+        >>> removing([-4, -7, -2, 1, 9]) # Found(-4) Delete -4, -7, -2 and 1
+        [9]
+        >>> removing([-3, -4, 5, -4, 1])  # Found(-3) Delete -2, -4 and 5. Found(-4) Delete -4 and 1
+        []
     """
-    # Break case? - No more negatives
-    # n = 0
-    # a = aList
-    # if len(a) == 1 and a[0] >= 0:
-    #     return aList
-
-    # elif a[0] < 0 and len(a)>2:
-    #     # If you can, remove the negative and the index next to it
-    #     print(f'found({a[0]},{a[1]})')
-    #     a.pop(0)
-    #     a.pop(0)
-    #     print(f'list: {a}, here and friend')
-    #     return removing(a)
-    # elif a[0] < 0:
-    #     # There is no index next to this one, pop it and return
-    #     print(f'found({a[0]})')
-    #     a.pop(0)
-    #     print(f'list: {a}, here and no friend')
-    #     return removing(a)
-    # else:
-    #     # Remove the index to progress, but add it back when all is returned
-    #     a.pop(0)
-    #     print(f'list: {a}, move on')
-    #     if len(removing(a)) == 1:
-    #         return aList
-    #     else:
-    #         return aList + removing(a)
-
+  
+    # Duplicate the list
     a = aList
 
+    # If the list is empty: Breaking condition
     if not a:
-        # The list is empty
         return
 
-    if ( len(a) >= 1 and a[0] >= 0 ):
-        # Temporarily remove index 0 but add it back after functions give back
-        return aList + removing(a)
+    # Remove the negative index and the one next to it.. If there are enough indices
     elif len(a) >= 2 and a[0] < 0:
-        # Remove friend and neighbour and call removing
         a.pop(0)
         a.pop(0)
-        if len(a) == 0:
+        return removing(a)
 
+    # Remove the negative index.. this is the last index
     elif len(a) == 1 and a[0] < 0:
         a.pop(0)
-        if len(a) == 0:
+        return removing(a)
 
-    return removing(a)
+    # Temporarily remove the index and add it back later
+    else:
+        return aList[0] + removing(a)
+
 
 def neighbor(n):
     """
@@ -100,11 +76,26 @@ def neighbor(n):
         >>> neighbor(2222466666625)
         24625
     """
-    ## YOUR CODE STARTS HERE
-    pass
+
+    a = str(n)
+
+    # Not enough characters to compare two: Breaking condition
+    if len(a) == 1:
+        return a[0]
+
+    # Check if there are enough indicies to check
+    if len(a) >= 2:
+
+        # The neighbours are the same, remove cuurent and move on
+        if a[0] == a[1]:
+            return neighbor( int(a[1:]) )
+
+        # The neighbours are not the same, remove cuurent and move on, but add current back later
+        elif a[0] != a[1]:
+            n = str(n)
+            return n[0] + neighbor( a[1:] )
+
     
-
-
 def missedChar(txt1, txt2):
     """
         >>> missedChar("owl", "howl")
@@ -116,23 +107,20 @@ def missedChar(txt1, txt2):
         >>> missedChar('at','treatise')
         'treise'
     """
-    ## YOUR CODE STARTS HERE
-    pass
 
-    # print(type(aList))
+    # Check if the first str is in the second str
+    if txt1 in txt2:    
 
-    # if len(aList) == 0:
-    #     return aList
+        # If this character and the next are the same, we can remove the first one
+        if txt1[0] == txt2[0] and txt1[1] == txt2[1]:
+            txt1[1:]
+            txt2[1:]
+            return missedChar(txt1, txt2)
 
-    # if aList[n] < 0:
-    #     aList.pop(n)
-    #     if len(aList) >= n+2:
-    #         aList.pop(n+1)
 
-    #     if len(aList) > n+1:
-    #         return removing(a)
-    # else:
-    #     s = removing( aList.pop(n) )
-    #     p = [ aList[n] ]
-    #     p.append(s)
-    #     return p
+    # The first str is no longer in the second str: Break condition
+    else:
+        if len(txt2) > 0:
+            return txt2
+
+
