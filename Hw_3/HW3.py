9eba4f901eb800ed005f5532121288429195ec6d
Dominic Assia <dominicassia@gmail.com>
2020-10-18 11:55:16 -0400

    working on postfix conversion

--- Hw_3/HW3.py
+++ Hw_3/HW3.py
@@ -94,9 +94,12 @@ class Stack:
 #=============================================== Part II ==============================================
 
 class Calculator:
+
+    # Infix: 4 + 3 - 2 * 4
+    # Postfix: 4324*-+
+
     def __init__(self):
         self.__expr = None
-
 
     @property
     def getExpr(self):
@@ -110,13 +113,12 @@ class Calculator:
             return None
 
     def isNumber(self, txt):
-        # YOUR CODE STARTS HERE
-        pass
-
-
-
-
-
+        # Returns True if txt can be converted to a float
+        try:
+            float(txt)
+            return True
+        except ValueError:
+            return False
 
 
     def _getPostfix(self, txt):
@@ -158,142 +160,222 @@ class Calculator:
             >>> x._getPostfix('2 * 5% + 3 ^ + -2 + 1 + 4')
         '''
 
-        # YOUR CODE STARTS HERE
+        precedence = { '-': 0, '+': 1, '/': 2, '*': 3, '^': 4, '(': 5, ')': 6}
+        PF = ''
+
         postOp = Stack()
-        pass
+        
+        txt = txt.split()   
+
+        for i in range(len(txt)):      
+
+            print('txt[i]: (top)', txt[i], '\n')
+
+            if txt[i] in precedence:
+
+                print(postOp, '\n')
+                print('PF (top):', PF, '\n')
+
+                if txt[i] == ')':
+
+                    x = txt[i]
+                    while x != '(' and not postOp.isEmpty:
+
+                        x = postOp.pop()
+                        PF += x + ' '
+
+                    # Mismatching parenthesis
+                    print('[ERROR]: Mismatching parenthesis')
 
 
-    @property
-    def calculate(self):
-        '''
-            Required: calculate must call postfix
-                      calculate must create and use a Stack to compute the final result as shown in the video lecture
-            >>> x=Calculator()
-            >>> x.setExpr('4 + 3 - 2')
-            >>> x.calculate
-            5.0
-            >>> x.setExpr('-2 + 3.5')
-            >>> x.calculate
-            1.5
-            >>> x.setExpr('4 + 3.65 - 2 / 2')
-            >>> x.calculate
-            6.65
-            >>> x.setExpr('23 / 12 - 223 + 5.25 * 4 * 3423')
-            >>> x.calculate
-            71661.91666666667
-            >>> x.setExpr(' 2 - 3 * 4')
-            >>> x.calculate
-            -10.0
-            >>> x.setExpr(' 3 * ( ( ( 10 - 2 * 3 ) ) )')
-            >>> x.calculate
-            12.0
-            >>> x.setExpr('8 / 4 * ( 3 - 2.45 * ( 4 - 2 ^ 3 ) ) + 3')
-            >>> x.calculate
-            28.6
-            >>> x.setExpr('2 * ( 4 + 2 * ( 5 - 3 ^ 2 ) + 1 ) + 4')
-            >>> x.calculate
-            -2.0
-            >>> x.setExpr(' 2.5 + 3 * ( 2 + ( 3.0 ) * ( 5 ^ 2 - 2 * 3 ^ ( 2 ) ) * ( 4 ) ) * ( 2 / 8 + 2 * ( 3 - 1 / 3 ) ) - 2 / 3 ^ 2')
-            >>> x.calculate
-            1442.7777777777778
+                # Push to stack if empty
+                elif postOp.isEmpty():
+
+                    if txt[i] == '^':
+                        postOp.push('**')
+                    else:
+                        postOp.push(txt[i])
+
+                    print(postOp)
+                    print('PF (empty);', PF, '\n')
+
+                # Otherwise check for precedence pop until find lower precedence
+                else:
+
+                    print(postOp)
+                    print('PF (else);', PF, '\n')
+
+                    o = 0
+                    z = postOp.pop()
+
+                    while precedence[z] > precedence[txt[i]]:
+
+                        if not o:
+                            PF += z + ' '
+                        o += 1
+
+                        if not postOp.isEmpty():
+                            z = postOp.pop()
+                            PF += z + ' '
+                        else:
+                            break
+
+                        print('PF (bottom);', PF, '\n')
+                        print(postOp)
+                        print('txt[i]: (bottom)', txt[i], '\n')
+
+                    if txt[i+1] not in precedence:
+                        PF += txt[i] + ' '
+
+            elif self.isNumber(txt[i]):
+
+                # Add to return string
+                PF += str(float(txt[i])) + ' '
+
+            # Invalid cases
+            else:
+
+                if not txt[i].isnumeric() and txt[i] not in precedence:
+                    print('[ERROR]: Invalid operator')
+
+                # More cases to come
+
+        # Remove the addistional space at the end
+        return PF[:len(PF)-1]
+
+
+
+#     @property
+#     def calculate(self):
+#         '''
+#             Required: calculate must call postfix
+#                       calculate must create and use a Stack to compute the final result as shown in the video lecture
+#             >>> x=Calculator()
+#             >>> x.setExpr('4 + 3 - 2')
+#             >>> x.calculate
+#             5.0
+#             >>> x.setExpr('-2 + 3.5')
+#             >>> x.calculate
+#             1.5
+#             >>> x.setExpr('4 + 3.65 - 2 / 2')
+#             >>> x.calculate
+#             6.65
+#             >>> x.setExpr('23 / 12 - 223 + 5.25 * 4 * 3423')
+#             >>> x.calculate
+#             71661.91666666667
+#             >>> x.setExpr(' 2 - 3 * 4')
+#             >>> x.calculate
+#             -10.0
+#             >>> x.setExpr(' 3 * ( ( ( 10 - 2 * 3 ) ) )')
+#             >>> x.calculate
+#             12.0
+#             >>> x.setExpr('8 / 4 * ( 3 - 2.45 * ( 4 - 2 ^ 3 ) ) + 3')
+#             >>> x.calculate
+#             28.6
+#             >>> x.setExpr('2 * ( 4 + 2 * ( 5 - 3 ^ 2 ) + 1 ) + 4')
+#             >>> x.calculate
+#             -2.0
+#             >>> x.setExpr(' 2.5 + 3 * ( 2 + ( 3.0 ) * ( 5 ^ 2 - 2 * 3 ^ ( 2 ) ) * ( 4 ) ) * ( 2 / 8 + 2 * ( 3 - 1 / 3 ) ) - 2 / 3 ^ 2')
+#             >>> x.calculate
+#             1442.7777777777778
             
 
-            # In invalid expressions, you might print an error message, but code must return None, adjust doctest accordingly
-            >>> x.setExpr(" 4 + + 3 + 2") 
-            >>> x.calculate
-            >>> x.setExpr("4  3 + 2")
-            >>> x.calculate
-            >>> x.setExpr('( 2 ) * 10 - 3 * ( 2 - 3 * 2 ) )')
-            >>> x.calculate
-            >>> x.setExpr('( 2 ) * 10 - 3 * / ( 2 - 3 * 2 )')
-            >>> x.calculate
-            >>> x.setExpr(' ) 2 ( * 10 - 3 * ( 2 - 3 * 2 ) ')
-            >>> x.calculate
-        '''
+#             # In invalid expressions, you might print an error message, but code must return None, adjust doctest accordingly
+#             >>> x.setExpr(" 4 + + 3 + 2") 
+#             >>> x.calculate
+#             >>> x.setExpr("4  3 + 2")
+#             >>> x.calculate
+#             >>> x.setExpr('( 2 ) * 10 - 3 * ( 2 - 3 * 2 ) )')
+#             >>> x.calculate
+#             >>> x.setExpr('( 2 ) * 10 - 3 * / ( 2 - 3 * 2 )')
+#             >>> x.calculate
+#             >>> x.setExpr(' ) 2 ( * 10 - 3 * ( 2 - 3 * 2 ) ')
+#             >>> x.calculate
+#         '''
 
-        if not isinstance(self.__expr,str) or len(self.__expr)<=0:
-            print("Argument error in calculate")
-            return None
+#         if not isinstance(self.__expr,str) or len(self.__expr)<=0:
+#             print("Argument error in calculate")
+#             return None
 
-        calculateStack=Stack()
+#         calculateStack=Stack()
 
-        # YOUR CODE STARTS HERE
-        pass
+#         # YOUR CODE STARTS HERE
+#         pass
 
-#=============================================== Part III ==============================================
+# #=============================================== Part III ==============================================
 
-class AdvancedCalculator:
-    '''
-    >>> C = AdvancedCalculator()
-    >>> C.states == {}
-    True
-    >>> C.setExpression('a = 5;b = 7 + a;a = 7;c = a + b;c = a * 0;return c')
-    >>> C.calculateExpressions() == {'a = 5': {'a': 5.0}, 'b = 7 + a': {'a': 5.0, 'b': 12.0}, 'a = 7': {'a': 7.0, 'b': 12.0}, 'c = a + b': {'a': 7.0, 'b': 12.0, 'c': 19.0}, 'c = a * 0': {'a': 7.0, 'b': 12.0, 'c': 0.0}, '_return_': 0.0}
-    True
-    >>> C.states == {'a': 7.0, 'b': 12.0, 'c': 0.0}
-    True
-    >>> C.setExpression('x1 = 5;x2 = 7 * ( x1 - 1 );x1 = x2 - x1;return x2')
-    >>> C.states == {}
-    True
-    >>> C.calculateExpressions() == {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 28.0}
-    True
-    >>> print(C.calculateExpressions())
-    {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 28.0}
-    >>> C.states == {'x1': 23.0, 'x2': 28.0}
-    True
-    >>> C.setExpression('x1 = 5 * 5 + 97;x2 = 7 * ( x1 / 2 );x1 = x2 * 7 / x1;return x1')
-    >>> C.calculateExpressions() == {'x1 = 5 * 5 + 97': {'x1': 122.0}, 'x2 = 7 * ( x1 / 2 )': {'x1': 122.0, 'x2': 427.0}, 'x1 = x2 * 7 / x1': {'x1': 24.5, 'x2': 427.0}, '_return_': 24.5}
-    True
-    >>> C.states == {'x1': 24.5, 'x2': 427.0}
-    True
-    >>> C.setExpression('A = 1;B = A + 9;C = A + B;A = 20;D = A + B + C;return D')
-    >>> C.calculateExpressions() == {'A = 1': {'A': 1.0}, 'B = A + 9': {'A': 1.0, 'B': 10.0}, 'C = A + B': {'A': 1.0, 'B': 10.0, 'C': 11.0}, 'A = 20': {'A': 20.0, 'B': 10.0, 'C': 11.0}, 'D = A + B + C': {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}, '_return_': 41.0}
-    True
-    >>> C.states == {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}
-    True
-    '''
-    def __init__(self):
-        self.expressions = ''
-        self.states = {}
+# class AdvancedCalculator:
+#     '''
+#     >>> C = AdvancedCalculator()
+#     >>> C.states == {}
+#     True
+#     >>> C.setExpression('a = 5;b = 7 + a;a = 7;c = a + b;c = a * 0;return c')
+#     >>> C.calculateExpressions() == {'a = 5': {'a': 5.0}, 'b = 7 + a': {'a': 5.0, 'b': 12.0}, 'a = 7': {'a': 7.0, 'b': 12.0}, 'c = a + b': {'a': 7.0, 'b': 12.0, 'c': 19.0}, 'c = a * 0': {'a': 7.0, 'b': 12.0, 'c': 0.0}, '_return_': 0.0}
+#     True
+#     >>> C.states == {'a': 7.0, 'b': 12.0, 'c': 0.0}
+#     True
+#     >>> C.setExpression('x1 = 5;x2 = 7 * ( x1 - 1 );x1 = x2 - x1;return x2')
+#     >>> C.states == {}
+#     True
+#     >>> C.calculateExpressions() == {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 28.0}
+#     True
+#     >>> print(C.calculateExpressions())
+#     {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 28.0}
+#     >>> C.states == {'x1': 23.0, 'x2': 28.0}
+#     True
+#     >>> C.setExpression('x1 = 5 * 5 + 97;x2 = 7 * ( x1 / 2 );x1 = x2 * 7 / x1;return x1')
+#     >>> C.calculateExpressions() == {'x1 = 5 * 5 + 97': {'x1': 122.0}, 'x2 = 7 * ( x1 / 2 )': {'x1': 122.0, 'x2': 427.0}, 'x1 = x2 * 7 / x1': {'x1': 24.5, 'x2': 427.0}, '_return_': 24.5}
+#     True
+#     >>> C.states == {'x1': 24.5, 'x2': 427.0}
+#     True
+#     >>> C.setExpression('A = 1;B = A + 9;C = A + B;A = 20;D = A + B + C;return D')
+#     >>> C.calculateExpressions() == {'A = 1': {'A': 1.0}, 'B = A + 9': {'A': 1.0, 'B': 10.0}, 'C = A + B': {'A': 1.0, 'B': 10.0, 'C': 11.0}, 'A = 20': {'A': 20.0, 'B': 10.0, 'C': 11.0}, 'D = A + B + C': {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}, '_return_': 41.0}
+#     True
+#     >>> C.states == {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}
+#     True
+#     '''
+#     def __init__(self):
+#         self.expressions = ''
+#         self.states = {}
 
-    def setExpression(self, expression):
-        self.expressions = expression
-        self.states = {}
+#     def setExpression(self, expression):
+#         self.expressions = expression
+#         self.states = {}
 
-    def isVariable(self, word):
-        '''
-            >>> C = AdvancedCalculator()
-            >>> C.isVariable('volume')
-            True
-            >>> C.isVariable('4volume')
-            False
-            >>> C.isVariable('volume2')
-            True
-            >>> C.isVariable('vol%2')
-            False
-        '''
-        # YOUR CODE STARTS HERE
-        pass
+#     def isVariable(self, word):
+#         '''
+#             >>> C = AdvancedCalculator()
+#             >>> C.isVariable('volume')
+#             True
+#             >>> C.isVariable('4volume')
+#             False
+#             >>> C.isVariable('volume2')
+#             True
+#             >>> C.isVariable('vol%2')
+#             False
+#         '''
+#         # YOUR CODE STARTS HERE
+#         pass
        
 
-    def replaceVariables(self, expr):
-        '''
-            >>> C = AdvancedCalculator()
-            >>> C.states = {'x1': 23.0, 'x2': 28.0}
-            >>> C.replaceVariables('1')
-            '1'
-            >>> C.replaceVariables('7 * ( x1 - 1 )')
-            '7 * ( 23.0 - 1 )'
-            >>> C.replaceVariables('x2 - x1')
-            '28.0 - 23.0'
-        '''
+#     def replaceVariables(self, expr):
+#         '''
+#             >>> C = AdvancedCalculator()
+#             >>> C.states = {'x1': 23.0, 'x2': 28.0}
+#             >>> C.replaceVariables('1')
+#             '1'
+#             >>> C.replaceVariables('7 * ( x1 - 1 )')
+#             '7 * ( 23.0 - 1 )'
+#             >>> C.replaceVariables('x2 - x1')
+#             '28.0 - 23.0'
+#         '''
 
-        # YOUR CODE STARTS HERE
-        pass
+#         # YOUR CODE STARTS HERE
+#         pass
 
     
-    def calculateExpressions(self):
-        self.states = {} 
-        calc = Calculator()
-        # YOUR CODE STARTS HERE
-        pass
+#     def calculateExpressions(self):
+#         self.states = {} 
+#         calc = Calculator()
+#         # YOUR CODE STARTS HERE
+#         pass
